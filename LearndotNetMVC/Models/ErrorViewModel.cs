d9e6ea18e2ee4939e1e77fbd1596b32da9bf02f0
Toan <toan.trannhu.sistrain@gmail.com>
2023-08-11 23:22:24 +0700

    init

---
+++ LearndotNetMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LearndotNetMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
